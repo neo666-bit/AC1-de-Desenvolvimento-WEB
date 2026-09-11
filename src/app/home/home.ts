56fafa7a21ef13ac44daaf589d86a19f2fc0527c
neo666-bit <marciano.julinho@gmail.com>
2026-09-10 21:07:48 -0300

    AC1 Completa

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+ 
+
 
 @Component({
-  imports: [],
+  imports: [RouterOutlet, FormsModule],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+    lista :string[] = ['organização', 'mercado', 'academia', 'estudos', 'trabalho']
+    selLista: string = ''
+    novoRecado : string = ''
+    lido: boolean = false
+    curtidas: number = 0
+
+    
+
+    adicionarRecado() {
+        if (this.novoRecado.trim() !== '') {
+            this.lista.push(this.novoRecado);
+            this.novoRecado = '';
+        }
+
+    }
+
+    curtir(){
+        this.curtidas++;
+    }
+
+}
